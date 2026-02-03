60e4f2f43f7f55e6c2cc11a274e657e4fe38f69b
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-02-03 14:46:46 +0300

    add js file

---
+++ js.js
@@ -0,0 +1,97 @@
+// Task 1 
+// let age = 24
+// let age2 = age+5
+// console.log(`After 5 years I will be: ${age2}`)
+
+// task 2
+// let a = 10
+// let b = 3 
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// task 3
+// let age = +prompt("apki age kia hy?");
+// if(age >=18){
+// console.log("You can vote");
+// }else{
+//   console.log("You are underage");
+// }
+
+// task 4
+// let studentNumber = prompt("exam me kitny marks ay?");
+// if(studentNumber >90){
+//     console.log("Grade A");
+// }else if(studentNumber >80){
+//      console.log("Grade B");
+// }else if(studentNumber >70){
+//      console.log("Grade C");
+// }else{
+//     console.log("Fail");
+// }
+
+// task 5 
+// for(let i = 1 ; i<11 ; i++){
+//     console.log(i)
+// }
+
+// task 6
+// for(let i = 1 ; i<11 ; i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// task 7
+// function addNumbers(a, b){
+// console.log(a+b)
+// }
+// addNumbers(4,6);
+
+// task 8
+// function isEven(a){
+// if(a%2 === 0){
+//     console.log(`${a} is Even number`);
+// }else{
+//   console.log(`${a} is odd number`);  
+// }
+// }
+// isEven(3);
+
+// task 9
+// let naam = ["Ali", "Sara", "Ahmed"];
+// console.log(naam[0]);
+// console.log(naam[2]);
+// console.log(naam.length);
+
+// task 10
+let naam = ["Ali", "Sara", "Ahmed"];
+
+
+// task 11
+// let student = {
+// name : "Raza" ,
+// age : 24,
+// course : "Math"
+// }
+// console.log(student.name);
+// console.log(student.course);
+
+// task 12
+// let student = {
+// name : "Raza" ,
+// age : 24,
+// course : "Math",
+// marks: 55,
+// }
+// console.log(student.name);
+// console.log(student.course);
+// if(student.marks>50){
+//     console.log("pass");
+// }else{
+//     console.log("Fail");
+// }
+
+
